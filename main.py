aacaef0cbb1d3968e80e2d263f1ff34b2b403f7a
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-01-08 15:49:26 +0000

    Added While Loop Solution

--- main.py
+++ main.py
@@ -11,4 +11,14 @@ def valid_input(prompt:str):
 
 input_num = valid_input("Enter a positive integer: ")
 
+# Using list comprehension loop
 print( "\n".join( f"{input_num} x {i} = {i*input_num}" for i in range(1,11) ) )
+
+#Separate results by 3 lines
+print("\n" * 3)
+
+# Using while Loop
+counter = 0
+while counter <= 10:
+  print( f"{input_num} x {counter} = {counter*input_num}" )
+  counter += 1
